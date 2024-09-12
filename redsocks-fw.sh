1852633b285e46e5d975d3cf912bbc319df0b300
christianmerges <61514608+christianmerges@users.noreply.github.com>
2024-09-12 20:52:09 +0200

    Update redsocks-fw.sh
    Changed the logic of which traffic should go to redsocks. Now only traffic which is not going to the socks ip will go straight to wan.

--- redsocks-fw.sh
+++ redsocks-fw.sh
@@ -12,20 +12,17 @@ fw_setup() {
       iptables -t nat -A REDSOCKS -d $item -j RETURN
   done < /etc/redsocks-whitelist.txt
 
-  # We then told iptables to redirect all port 80 connections to the http-relay redsocks port and all other connections to the http-connect redsocks port.
+  # Redirect all TCP traffic to redsocks and all dns traffic to internal dns.
   iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
   iptables -t nat ! -d 114.114.114.114 -I REDSOCKS 1 -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:5533
 
 
-  # Finally we tell iptables to use the ‘REDSOCKS’ chain for all outgoing connection in the network interface ‘$DOCKER_NET′.
-  if [ -z "$DOCKER_NET" ]
-  then
-    #iptables -t nat -A OUTPUT  -j REDSOCKS
-    iptables -t nat -I OUTPUT 1 -j REDSOCKS
-  else
-    iptables -t nat -A OUTPUT  -i $DOCKER_NET -j REDSOCKS
-  fi
+  # Every Traffic which is not the socks server as destination is getting redirected to redsocks.
+  iptables --table nat --append OUTPUT ! --destination "$socksip" --jump REDSOCKS
+
+
 }
+
 
 ##########################
 # Clear the Firewall rules
